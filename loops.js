f58bf9c08fbcc32e033d1ef2d353aced482d41dc
Sean Huang <hyprul@ucla.edu>
2018-04-30 02:04:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,6 +19,13 @@ function whileLoop(n){
   return "done"
 }
 
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
 function doWhileLoop(array){
-  
+  do{
+    array.unshift()
+  } while ()
 }
